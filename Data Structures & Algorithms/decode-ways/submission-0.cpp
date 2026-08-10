f40a90d72ed551c79876dcb90eccca7718f7c3db
Samuel Aboderin <aboderinseun01@gmail.com>
2026-08-11 00:33:53 +0100

    Add: decode-ways - submission-0

---
+++ Data Structures & Algorithms/decode-ways/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.size();
+        vector<int> dp(n+1, 0);
+        if(n == 0 || s[0] == '0') return 0;
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i=2; i<=n; i++){
+            if(s[i-1] != '0'){
+                dp[i] += dp[i-1];
+            }
+            int twoDigits = ((s[i-2] - '0')*10 + s[i-1]-'0');
+            if(twoDigits >= 10 && twoDigits <= 26){
+                dp[i] += dp[i-2];
+            }
+        }
+        return dp[n];
+    }
+};
