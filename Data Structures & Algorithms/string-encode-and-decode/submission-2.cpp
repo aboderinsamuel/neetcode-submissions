a41f10d71a13a71315d5d35a385c944190d5b4fd
Samuel Aboderin <aboderinseun01@gmail.com>
2026-08-13 12:35:31 +0100

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string encoded = "";
+        for(string str : strs){
+            encoded += to_string(str.size()) + "#" + str;
+        }
+        return encoded;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> result;
+        int i = 0;
+        while(i < s.size()){
+            //j will move until it finds "#"
+            int j = i;
+            while(s[j] != '#') j++;
+            int length = stoi(s.substr(i, j-i));
+            j++;
+            result.push_back(s.substr(j, length));
+            i = j + length;
+        }
+        return result;
+    }
+};
