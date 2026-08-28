85bc52ccbe56d600c970274cef9e9a84893e5494
Samuel Aboderin <aboderinseun01@gmail.com>
2026-08-28 12:10:40 +0100

    Add: longest-common-prefix - submission-2

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty()) return "";
+
+        string base = strs[0];
+
+        for(int i=0; i<base.length(); i++){
+            char currentChar = base[i];
+        
+            for(int j=1; j<strs.size(); j++){
+                if(i >= strs[j].length() || strs[j][i] != currentChar){
+                    return base.substr(0, i);
+                }
+            }
+        }
+        return base;
+    }
+};
