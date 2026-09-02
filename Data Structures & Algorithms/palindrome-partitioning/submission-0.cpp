4289953b375b5d31775be3ba495eb20680c902fe
Samuel Aboderin <aboderinseun01@gmail.com>
2026-09-02 17:08:54 +0100

    Add: palindrome-partitioning - submission-0

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-0.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+
+    bool isPalindrome(string& s, int left, int right){
+        while(left <= right){
+            if(s[left++] != s[right--]) return false;
+        }
+        return true;
+    }
+
+    void backtrack(string& s, int start, vector<string>& current, vector<vector<string>>& result){
+        if(start == s.size()){
+            result.push_back(current);
+            return;
+        }
+
+        for(int end=start; end<s.size(); end++){
+            if(isPalindrome(s, start, end)){
+                current.push_back(s.substr(start, end-start+1));
+                backtrack(s, end+1, current, result);
+                current.pop_back();
+            }
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> result;
+        vector<string> current;
+        backtrack(s, 0, current, result);
+        return result;
+    }
+};
